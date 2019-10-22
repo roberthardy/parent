f9e9d21179fc07a9cb194505b8e8c7b06c06b05a
Robert Hardy <robert.hardy@gmail.com>
2019-10-22 20:36:43 +0100

    Added Parent solution

---
+++ Parent.Cli/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parent.Cli
+{
+    public class Class1
+    {
+    }
+}
